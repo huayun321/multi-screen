153ff77d423ce5e5e6511108b95065ee700bcfcc
huayun321 <huayun321@gmail.com>
2016-03-04 10:44:06 +0800

    修改serverUrl

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var config = require('config');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Multi-screen' });
+  res.render('index', { title: 'Multi-screen', serverUrl: config.serverUrl });
 });
 
 module.exports = router;
